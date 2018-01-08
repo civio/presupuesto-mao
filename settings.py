ecf32d6989b2863ab662975b51d334291d4feae2
Eduardo Sebastian <esebastian@altiora.es>
2018-01-08 09:50:51 +0100

    Update stats data

--- settings.py
+++ settings.py
@@ -99,10 +99,10 @@ ANALYTICS_ID            = 'UA-28946840-42'
 DATA_SOURCE_BUDGET      = ''
 
 # Setup Data Source Population link
-DATA_SOURCE_POPULATION  = ''
+DATA_SOURCE_POPULATION  = 'http://www.ine.es/jaxiT3/Tabla.htm?t=2860&L=0'
 
 # Setup Data Source Inflation link
-DATA_SOURCE_INFLATION   = ''
+DATA_SOURCE_INFLATION   = 'http://www.ine.es/jaxiT3/Tabla.htm?t=22350&L=0'
 
 # Setup Main Entity Web Url
 MAIN_ENTITY_WEB_URL     = 'http://www.ajmao.org/'
