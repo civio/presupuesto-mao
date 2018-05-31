7200369f5d43a99c4ba768357d121ca307cde653
Eduardo Sebastian <esebastian@altiora.es>
2018-05-31 07:54:28 +0200

    Remove obsolete programme codes
    civio/presupuesto-management#736

--- loaders/mao_budget_loader.py
+++ loaders/mao_budget_loader.py
@@ -1,35 +1,65 @@
 # -*- coding: UTF-8 -*-
 from budget_app.loaders import SimpleBudgetLoader
 
+expenses_mapping = {
+    '2018': {'ic_code': 0, 'fc_code': 1, 'full_ec_code': 3, 'description': 5, 'forecast_amount': 7, 'actual_amount': None},
+    '2017': {'ic_code': 3, 'fc_code': 4, 'full_ec_code': 5, 'description': 6, 'forecast_amount': 7, 'actual_amount': 13},
+    '2016': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 11},
+    '2015': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 9},
+    '2014': {'ic_code': 1, 'fc_code': 2, 'full_ec_code': 3, 'description': 4, 'forecast_amount': 5, 'actual_amount': 8},
+    '2013': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 9},
+    '2012': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 9},
+}
+
+income_mapping = {
+    '2018': {'full_ec_code': 0, 'description': 2, 'forecast_amount': 4, 'actual_amount': None},
+    '2017': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 9},
+    '2016': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 7},
+    '2015': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 8},
+    '2014': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 8},
+    '2013': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 8},
+    '2012': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 7},
+}
+
+programme_mapping = {
+    # old programme: new programme
+    '1550': '1532',     # Vies públiques
+    '1620': '1621',     # R.S.U. i neteja viària
+    '1690': '1711',     # Serveis comuns de ciutat i medi ambient -> Ciutat i medi ambient
+    '1695': '1652',     # Brigada d'obres
+    '1720': '1721',     # Medi ambient -> Medi ambient urbà
+    '2304': '2310',     # Atenció primària -> Asistència social primaria
+    '2305': '2311',     # Mediació social -> R.G.A.
+    '3120': '2311',     # R.G.A. -> R.G.A.
+    '3130': '3110',     # Sanitat
+    '3131': '3110',     # Servei de Llacer -> Sanitat
+    '3233': '3260',     # Escoles municipals
+    '3323': '3322',     # Arxiu i patrimoni
+    '3390': '3300',     # Serveis comuns del S.M.C. -> Cultura
+    '4301': '4310',     # Comerç
+    '4302': '4312',     # Mercats
+    '4304': '4320',     # Turisme
+    '9200': '9250',     # Oficina d’atenció al ciutadà
+    '9270': '9260',     # Comunicació
+    '9324': '9340',     # Gestió tributària i financera
+}
+
+programme_mapping_changes = {
+    # old programme: new programme
+    '1624': '1532',     # Vías públicas
+    '3120': '2311',     # R.G.A.
+    '3340': '3300',     # Cultura
+    '1651': '1711',     # Ciudad y medio ambiente
+}
 
 class MaoBudgetCsvMapper:
-    expenses_mapping = {
-        '2018': {'ic_code': 0, 'fc_code': 1, 'full_ec_code': 3, 'description': 5, 'forecast_amount': 7, 'actual_amount': None},
-        '2017': {'ic_code': 3, 'fc_code': 4, 'full_ec_code': 5, 'description': 6, 'forecast_amount': 7, 'actual_amount': 13},
-        '2016': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 11},
-        '2015': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 9},
-        '2014': {'ic_code': 1, 'fc_code': 2, 'full_ec_code': 3, 'description': 4, 'forecast_amount': 5, 'actual_amount': 8},
-        '2013': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 9},
-        '2012': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 9},
-    }
-
-    income_mapping = {
-        '2018': {'full_ec_code': 0, 'description': 2, 'forecast_amount': 4, 'actual_amount': None},
-        '2017': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 9},
-        '2016': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 7},
-        '2015': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 8},
-        '2014': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 8},
-        '2013': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 8},
-        '2012': {'full_ec_code': 2, 'description': 3, 'forecast_amount': 4, 'actual_amount': 7},
-    }
-
     def __init__(self, year, is_expense):
-        column_mapping = MaoBudgetCsvMapper.expenses_mapping
+        column_mapping = income_mapping
 
-        if not is_expense:
-            column_mapping = MaoBudgetCsvMapper.income_mapping
+        if is_expense:
+            column_mapping = expenses_mapping
 
-        mapping = column_mapping.get(year)
+        mapping = column_mapping.get(str(year))
 
         self.ic_code = mapping.get('ic_code')
         self.fc_code = mapping.get('fc_code')
@@ -40,31 +70,6 @@ class MaoBudgetCsvMapper:
 
 
 class MaoBudgetLoader(SimpleBudgetLoader):
-    # Programme codes have changed in 2015, due to new laws. Since the application expects a code-programme
-    # mapping to be constant over time, we are forced to amend budget data prior to 2015.
-    # See https://github.com/dcabo/presupuestos-aragon/wiki/La-clasificaci%C3%B3n-funcional-en-las-Entidades-Locales
-    programme_mapping = {
-        # old programme: new programme
-        '1550': '1624',     # Vies públiques
-        '1620': '1621',     # R.S.U. i neteja viària
-        '1690': '1651',     # Serveis comuns de ciutat i medi ambient -> Ciutat i medi ambient
-        '1695': '1652',     # Brigada d'obres
-        '1720': '1721',     # Medi ambient -> Medi ambient urbà
-        '2304': '2310',     # Atenció primària -> Asistència social primaria
-        '2305': '2311',     # Mediació social -> R.G.A.
-        '3130': '3110',     # Sanitat
-        '3131': '3110',     # Servei de Llacer -> Sanitat
-        '3233': '3260',     # Escoles municipals
-        '3323': '3322',     # Arxiu i patrimoni
-        '3390': '3340',     # Serveis comuns del S.M.C. -> Promoció cultural
-        '4301': '4310',     # Comerç
-        '4302': '4312',     # Mercats
-        '4304': '4320',     # Turisme
-        '9200': '9250',     # Oficina d’atenció al ciutadà
-        '9270': '9260',     # Comunicació
-        '9324': '9340',     # Gestió tributària i financera
-    }
-
     # make year data available in the class and call super
     def load(self, entity, year, path, status):
         self.year = year
@@ -90,9 +95,16 @@ class MaoBudgetLoader(SimpleBudgetLoader):
             # We got decimal values as input, so we normalize them at 4- and add leading zeroes when required
             fc_code = line[mapper.fc_code].split('.')[0].rjust(4, '0')
 
+            # Programme codes have changed in 2015, due to new laws. Since the application expects a code-programme
+            # mapping to be constant over time, we are forced to amend budget data prior to 2015.
+            # See https://github.com/dcabo/presupuestos-aragon/wiki/La-clasificaci%C3%B3n-funcional-en-las-Entidades-Locales
             # For pre 2015 budgets we may need to amend the programme code
             if int(self.year) < 2015:
-                fc_code = MaoBudgetLoader.programme_mapping.get(fc_code, fc_code)
+                fc_code = programme_mapping.get(fc_code, fc_code)
+
+            # For 2015 and following budgets there are still some programme codes we need to amend, due to some classification changes
+            if int(self.year) >= 2015:
+                fc_code = programme_mapping_changes.get(fc_code, fc_code)
 
             # Economic code
             # We got decimal values as input, so we normalize them at 7- and add leading zeroes when required
