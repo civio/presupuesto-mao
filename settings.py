e7039cb71163baa9be169d8901134f5f9137b745
Eduardo Sebastian <esebastian@altiora.es>
2018-10-01 08:11:23 +0200

    Enable payments section
    civio/presupuesto-management#788

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ PAYMENTS_LOADER = 'MaoPaymentsLoader'
 # ----------------------
 
 # Show Payments section in menu & home options. Default: False.
-# SHOW_PAYMENTS = True
+SHOW_PAYMENTS = True
 
 # Configure 'by area' payment breakdown. Default: ['area', 'payee', 'description']
 # PAYMENTS_BREAKDOWN_BY_AREA = ['area', 'payee', 'description']
