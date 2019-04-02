4add7aa2c9db7bdf69177a3143efa50ca36d12be
Eduardo Sebastian <esebastian@altiora.es>
2019-04-02 08:53:49 +0200

    Update 2018 budget data
    civio/presupuesto-management#888

--- loaders/mao_budget_loader.py
+++ loaders/mao_budget_loader.py
@@ -3,7 +3,7 @@ from budget_app.loaders import SimpleBudgetLoader
 
 expenses_mapping = {
     '2019': {'ic_code': 0, 'fc_code': 1, 'full_ec_code': 3, 'description': 5, 'forecast_amount': 7, 'actual_amount': None},
-    '2018': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 12},
+    '2018': {'ic_code': 3, 'fc_code': 4, 'full_ec_code': 5, 'description': 6, 'forecast_amount': 7, 'actual_amount': 10},
     '2017': {'ic_code': 3, 'fc_code': 4, 'full_ec_code': 5, 'description': 6, 'forecast_amount': 7, 'actual_amount': 13},
     '2016': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 11},
     '2015': {'ic_code': 2, 'fc_code': 3, 'full_ec_code': 4, 'description': 5, 'forecast_amount': 6, 'actual_amount': 9},
